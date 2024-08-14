b621a22cfa92895989dca63edfd7c72594c3c117
divyagurav <132669878+divyagurav@users.noreply.github.com>
2024-08-14 11:55:10 +0530

    Add files via upload
    styling Button and Inputs with Tailwind CSS

--- src/components/Input.js
+++ src/components/Input.js
@@ -5,9 +5,15 @@ export default function Input({ label, textarea, ...props }) {
         {label}
       </label>
       {textarea ? (
-        <textarea classname="w-full p-1 border-b-2 rounded  " {...props} />
+        <textarea
+          className="w-full p-1 border-b-2 rounded-sm  border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+          {...props}
+        />
       ) : (
-        <input {...props} />
+        <input
+          className="w-full p-1 border-b-2 rounded-sm  border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+          {...props}
+        />
       )}
     </p>
   );
