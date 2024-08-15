e50687583335463213f44b2241a1e94e74837964
divyagurav <132669878+divyagurav@users.noreply.github.com>
2024-08-15 19:12:06 +0530

    Handling Project Deletion

--- src/App.js
+++ src/App.js
@@ -55,10 +55,24 @@ function App() {
 
   console.log(projectsState);
 
+  const handleDeleteProject = (id) => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  };
+
   const selectedProject = projectsState.projects.find(
     (project) => project.id === projectsState.selectedProjectId
   );
-  let content = <SelectedProject project={selectedProject} />;
+  let content = (
+    <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
+  );
 
   if (projectsState.selectedProjectId === null) {
     content = (
